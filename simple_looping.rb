1cc2f1ed74e08f954e8bd90a86f983f670006710
Aliya Lewis <aliyalewis5@gmail.com>
2019-04-27 01:32:05 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,7 +13,7 @@ def loop_iterator(number_of_times)
 
 def times_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  7.times do
+  number_of_times.times do
     puts phrase
   end
 end
